492441b3f145adb9df6f519f42b04afe9d2c3213
abbyot <abigail.toghanro@stu.pfisd.net>
2021-01-12 23:24:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import difflib
+
+words= ['i','have','want','a','test','like','am','cheese','coding','sleeping','sandwich','burger']
+
+def word_check(s):
+  for word in s.casefold().split():
+    if word not in words:
+      suggestion= difflib.get_close_matches(word, words)
+      print(f'Did you mean {",".join(str(x)for x in suggestion)} instead of {word}?')
+
+
+s = input('Input a string: ')
+word_check(s)
+
+# mod from python.org, https://docs.python.org/3/library/difflib.html
